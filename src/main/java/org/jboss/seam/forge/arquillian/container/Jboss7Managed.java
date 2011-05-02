1696a040b62e7a50fcb51c772ebaaba9ee29fd4c
Andrew Lee Rubinger <alr@jboss.org>
2011-05-02 15:02:38 -0400

    Use ALR fork of the AS7 codebase

--- src/main/java/org/jboss/seam/forge/arquillian/container/Jboss7Managed.java
+++ src/main/java/org/jboss/seam/forge/arquillian/container/Jboss7Managed.java
@@ -6,6 +6,7 @@ import javax.inject.Named;
 import org.jboss.forge.project.Project;
 import org.jboss.forge.project.dependencies.DependencyBuilder;
 import org.jboss.forge.project.dependencies.ScopeType;
+import org.jboss.forge.project.facets.DependencyFacet;
 
 public class Jboss7Managed implements Container {
     @Inject @Named("arquillianVersion") String arquillianVersion;
@@ -15,18 +16,21 @@ public class Jboss7Managed implements Container {
 
     @Override public void installDependencies() {
         DependencyBuilder dep1 = DependencyBuilder.create()
-                .setGroupId("org.jboss.arquillian.container")
-                .setArtifactId("arquillian-jbossas-managed-7")
+                .setGroupId("org.jboss.as")
+                .setArtifactId("jboss-as-arquillian2-container-managed")
                 //.setVersion(arquillianVersion)
-                .setVersion("1.0.0-SNAPSHOT")
+                .setVersion("7.0.0.Beta4-jbw2011")
                 .setScopeType(ScopeType.TEST);
 
         DependencyBuilder dep2 = DependencyBuilder.create()
                 .setGroupId("org.jboss.as")
                 .setArtifactId("jboss-as-controller-client")
-                .setVersion("7.0.0.Beta3")
+                .setVersion("7.0.0.Beta4-jbw2011")
                 .setScopeType(ScopeType.TEST);
 
-        builder.addProfile("jbossas-managed-7", dep1, dep2);
+        // Add dependencies
+        final DependencyFacet facet = project.getFacet(DependencyFacet.class);
+        facet.addDependency(dep1);
+        facet.addDependency(dep2);
     }
 }
